8e2c3d676c1da31d36f596b6e0d94c357c997fd7
much luv bruv <novak7amg@gmail.com>
2021-09-10 02:39:04 +0200

    express init

--- node/vue_front/src/store/index.js
+++ node/vue_front/src/store/index.js
@@ -5,14 +5,31 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-
+        proizvodi: [],
     },
     mutations: {
-
+        get_proizvodi: function (state, proizvodi) {
+            state.proizvodi = proizvodi
+        },
     },
-
     actions: {
 
-
-    }
+        load_proizvodi: function ({ commit }) {
+            fetch(  '/proizvodi', { method: 'get' })
+                .then((response) => {
+                    if (!response.ok)
+                        throw response;
+                    return response.json()
+                }).then((jsonData) => {
+                commit('get_proizvodi', jsonData)
+            }).catch((error) => {
+                if (typeof error.text === 'function')
+                    error.text().then((errorMessage) => {
+                        alert(errorMessage);
+                    });
+                else
+                    alert(error);
+            });
+        },
+    },
 })
--- node/vue_front/src/views/Proizvodi.vue
+++ node/vue_front/src/views/Proizvodi.vue
@@ -2,16 +2,49 @@
 
   <div class="proizvodi">
     <h1>Proizvodi</h1>
+    <div style="height: 4rem">asdasda</div>
+    <div class="tabela_proizvoda">
+      <b-table
+          hover v-if="proizvodi.length"
+          sticky-header="800px"
+          :items="proizvodi"
+          :fields="fields"
+          head-variant="light"
+      >
+      </b-table>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import { mapState, mapActions } from 'vuex';
 
 export default {
-  name: 'Proizvodi',
-  components: {
+  name: "Proizvodi",
+  computed: {
+    ...mapState(['proizvodi'])
+  },
+  data() {
+    return {
+      fields: [
+        { key: 'id' },
+        { key: 'ime' },
+        { key: 'opis' },
+        { key: 'stanje' },
+        { key: 'cena' },
+      ]
+    }
+  },
+  methods: {
+    ...mapActions(['load_proizvodi']),
+
+  },
+
+  mounted: function() {
+    this.load_proizvodi();
+
   }
 }
 </script>
